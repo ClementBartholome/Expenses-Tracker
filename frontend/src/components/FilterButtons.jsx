c13f933b13a7be6ec7317b1517e6635567e20175
ClementBartholome <123072377+ClementBartholome@users.noreply.github.com>
2023-08-30 18:44:22 +0200

    comments

--- frontend/src/components/FilterButtons.jsx
+++ frontend/src/components/FilterButtons.jsx
@@ -22,26 +22,26 @@ export default function FilterButtons() {
   };
 
   const [isDragging, setIsDragging] = useState(false);
-  const [startX, setStartX] = useState(0);
-  const [startScrollLeft, setStartScrollLeft] = useState(0);
+  const [startX, setStartX] = useState(0); // Initial X position of the mouse pointer or touch event
+  const [startScrollLeft, setStartScrollLeft] = useState(0); // Initial horizontal scroll position
 
-  // Event handler when the mouse button is pressed
+  // Event handler when the mouse button is pressed or touch event starts
   const handleMouseDown = (e) => {
     setIsDragging(true);
-    setStartX(e.clientX || e.touches[0].clientX);
-    setStartScrollLeft(filterButtonsContainerRef.current.scrollLeft);
+    setStartX(e.clientX || e.touches[0].clientX); // Store initial X position of mouse or touch event
+    setStartScrollLeft(filterButtonsContainerRef.current.scrollLeft); // Store initial scroll position
   };
 
-  // Event handler when the mouse button is released
+  // Event handler when the mouse button is released or touch event ends
   const handleMouseUp = () => {
     setIsDragging(false);
   };
 
-  // Event handler when the mouse is moving
+  // Event handler when the mouse is moving or touch event is moving
   const handleMouseMove = (e) => {
-    if (!isDragging) return;
-    const deltaX = ((e.clientX || e.touches[0].clientX) - startX) * 2; // Adjust scrolling speed
-    filterButtonsContainerRef.current.scrollLeft = startScrollLeft - deltaX;
+    if (!isDragging) return; // If not dragging, exit
+    const deltaX = ((e.clientX || e.touches[0].clientX) - startX) * 2; // Calculate change in X position, adjust scrolling speed
+    filterButtonsContainerRef.current.scrollLeft = startScrollLeft - deltaX; // Update the scroll position based on the calculated change
   };
 
   return (
