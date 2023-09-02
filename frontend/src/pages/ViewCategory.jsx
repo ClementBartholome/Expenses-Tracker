a564b600dc7f041a7a0abfb82bd5685131cd88e3
Clément Bartholomé <clementoss@gmail.com>
2023-09-03 00:07:01 +0200

    feat: display last 30 days total expenses for a category + monthly average

--- frontend/src/pages/ViewCategory.jsx
+++ frontend/src/pages/ViewCategory.jsx
@@ -16,6 +16,8 @@ export default function ViewCategory() {
 
   const [categoryTotals, setCategoryTotals] = useState({});
   const [categoryExpenses, setCategoryExpenses] = useState([]);
+  const [last30DaysTotal, setLast30DaysTotal] = useState(0); // State to store the total for the last 30 days
+  const [monthlyAverage, setMonthlyAverage] = useState(0); // State to store the average monthly total
 
   useEffect(() => {
     // Function to calculate the total expenses for a category for each month
@@ -43,11 +45,31 @@ export default function ViewCategory() {
         // Get the formatted month name using the formatMonthToFrench function
         const monthName = formatMonthToFrench(month);
 
-        // Store the total in the totals object
+        // Store the total for the month in the totals object
         totals[monthName] = total;
       }
 
       setCategoryExpenses(localCategoryExpenses);
+
+      // Calculate the total for the last 30 days
+      const last30DaysExpenses = localCategoryExpenses.filter((expense) => {
+        const thirtyDaysAgo = new Date();
+        thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30);
+        return new Date(expense.date) >= thirtyDaysAgo;
+      });
+      const last30DaysTotal = calculateTotalExpenses(last30DaysExpenses);
+      setLast30DaysTotal(last30DaysTotal);
+
+      // Calculate the average monthly expense
+      // Calculate the average monthly expense for the last six months
+      const lastSixMonths = Object.keys(totals).slice(-6);
+      const totalLastSixMonths = lastSixMonths.reduce(
+        (acc, month) => acc + totals[month],
+        0
+      );
+      const monthlyAverage = totalLastSixMonths / lastSixMonths.length;
+
+      setMonthlyAverage(monthlyAverage);
 
       // Transform the data for Chart.js
       const months = [...Object.keys(totals)]; // Extract month names
@@ -86,6 +108,18 @@ export default function ViewCategory() {
         onClick={() => window.history.back()}
       />
       <h2 className="margin-layout">{category}</h2>
+      <div className="category-cards">
+        {/* Display the total for the last 30 days */}
+        <div className="card margin-layout wd-50 fit-content">
+          <h3>30 derniers jours</h3>
+          <p className="bold">{formatAmount(last30DaysTotal)}</p>
+        </div>
+        {/* Display the average monthly expense */}
+        <div className="card margin-layout wd-50 fit-content">
+          <h3>Moyenne Mensuelle</h3>
+          <p className="bold">{formatAmount(monthlyAverage)}</p>
+        </div>
+      </div>
       <ExpenseChart data={categoryTotals} />
       <h2 className="margin-layout">Dernières dépenses</h2>
       <ul className="mb-20">
