c78be8ba988eb0d9c6ae66a93640e38d35a2e595
Clément Bartholomé <clementoss@gmail.com>
2023-09-02 22:35:08 +0200

    cors config

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+
+header("Access-Control-Allow-Headers: Content-Type");
+
+header("Content-Type: application/json");
+
 session_start();
 
 require_once "./backend/Config/autoload.php";
