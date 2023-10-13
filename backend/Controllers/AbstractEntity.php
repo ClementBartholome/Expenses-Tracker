358b89bef7e720b989abf8a7d83e977009b537c6
Clément Bartholomé <clementoss@gmail.com>
2023-10-13 11:24:33 +0200

    comments

--- backend/Controllers/AbstractEntity.php
+++ backend/Controllers/AbstractEntity.php
@@ -2,11 +2,15 @@
 
 abstract class AbstractEntity {
     public function hydrate(array $data) {
-    foreach ($data as $key => $value) {
-        $method = 'set' . ucfirst($key);
-        if (method_exists($this, $method)) {
-            $this->$method($value);
+        // Loop through the associative array of data
+        foreach ($data as $key => $value) {
+            // Generate the setter method name based on the property name
+            $method = 'set' . ucfirst($key);
+            // Check if the setter method exists in the current class
+            if (method_exists($this, $method)) {
+                // If the setter method exists, call it with the corresponding value
+                $this->$method($value);
+            }
         }
     }
-}
 }
